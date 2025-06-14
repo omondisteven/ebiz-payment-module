53255c72d49717a0cd03b520a5f8a0f420f56dde
omondisteven <omondisteven@gmail.com>
2025-06-14 04:51:09 +0300

    second commit after setting up app

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1neC0h2J9qcUpiBFZcI1fx7zgkg12Y_s",
-  authDomain: "ebizdb-9042d.firebaseapp.com",
-  projectId: "ebizdb-9042d",
-  storageBucket: "ebizdb-9042d.firebasestorage.app",
-  messagingSenderId: "1004547674536",
-  appId: "1:1004547674536:web:8bc3f298af30d45020cd9d",
-  measurementId: "G-YMGL338LP2"
+  apiKey: "AIzaSyA0NPtX2ZOi9nlAfLIcRg1C4VcnjgfGezw",
+  authDomain: "ebiz-v2.firebaseapp.com",
+  projectId: "ebiz-v2",
+  storageBucket: "ebiz-v2.firebasestorage.app",
+  messagingSenderId: "617164733312",
+  appId: "1:617164733312:web:d0d98d671c753a2c96df65",
+  measurementId: "G-4TZC0LJK9X"
 };
 
 // Initialize Firebase
